c85b412fbc1120ceea5933aebaf3c765cf6cc16d
Randi Schreiner <mimirandi21@gmail.com>
2021-01-08 16:05:12 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+	let todoForm = document.querySelector("#create-task-form");
+	let todoInput = document.querySelector("#new-task-description");
+	let todoList = document.querySelector("#tasks");
+
+	todoForm.addEventListener("submit", handleSubmit);
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		let todo = todoInput.value;
+		let todoHTML = `<li>${todo}<button class="delete-btn">X</button></li>`;
+		todoList.innerHTML += todoHTML;
+		todoInput.value = "";
+	}
+	todoList.addEventListener("click", handleDeleteItem);
+	function handleDeleteItem(e) {
+		e.target.parentElement.remove();
+	}
 });
